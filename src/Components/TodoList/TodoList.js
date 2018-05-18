0ae1c119da91a016ed54162631f4a6c186b80b45
Raul Gutierrez <rdgutierrez@gmail.com>
2018-05-17 17:12:17 -0700

    refactored TodoList into a pure component since it wasn't handling any state

--- src/Components/TodoList/TodoList.js
+++ src/Components/TodoList/TodoList.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TodoListItem from '../TodoListItem/TodoListItem';
 
-class TodoList extends Component {
-  render() {
-    var items = this.props.items.map((item, index) => {
-      return (
-        <TodoListItem
-          key={index}
-          item={item}
-          index={index}
-          removeItem={this.props.removeItem}
-          markTodoDone={this.props.markTodoDone}
-        />
-      );
-    });
-    return <ul className="list-group"> {items} </ul>;
-  }
-}
+const TodoList = props => {
+  let items = props.items.map((item, index) => (
+    <TodoListItem
+      key={index}
+      item={item}
+      index={index}
+      removeItem={props.removeItem}
+      markTodoDone={props.markTodoDone}
+    />
+  ));
+
+  return <ul className="list-group"> {items} </ul>;
+};
 
 export default TodoList;
